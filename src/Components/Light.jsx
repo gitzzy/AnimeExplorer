7d1cd16d4eb707bb6c69cdcb046a316787f6c9db
gitzzy <tyagidevansh10@gmail.com>
2025-09-09 18:18:11 +0530

    loa

--- src/Components/Light.jsx
+++ src/Components/Light.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Light() {
   return (
-    <div>
-      <h1>Letter of Apology</h1>
+    <div className='border p-5'>
+      <div className='text-xl text-extrabold text-center'>Letter of Apology</div>
       <br>
       </br>
       <p>Yo Light ✨,
@@ -11,10 +11,15 @@ export default function Light() {
 I know I messed up and it kinda pissed you off, and I just wanna say I’m sorry fr. 🙏
 Didn’t mean to hurt you or make you feel some type of way — you’re too important to me for that.
 I value our vibe a lot, and I don’t wanna let something small mess up the bond we got.
+<br></br>
+<br></br>
 
 So, let’s not stay stuck on the L. Let me make it up to you — boba/food/whatever you’re down for, my treat. 🫶
-
+<br></br>
+<br></br>
 Stay chill, bestie, don’t ghost me forever 😭.
+<br></br>
+<br></br>
 
 – Devansh Tyagi</p>
     </div>
