f570135a426aab8d107ac25cb34af56765e09194
gitzzy <tyagidevansh10@gmail.com>
2025-09-09 18:50:48 +0530

    analytics fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,33 @@
 import './App.css'
 import Banner from './Components/Banner'
 import Navbar from './Components/Navbar'
-import {BrowserRouter, Route, Routes,} from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Watchlist from './Components/Watchlist'
 import Animes from './Components/Animes'
-import Test from './Components/Test'
-import { useState } from 'react'
 import Light from './Components/Light'
-import { Analytics } from "@vercel/analytics/next"
+import { useState } from 'react'
 
 function App() {
-
-  let [watchlist,setWatch] = useState([])
+  let [watchlist, setWatch] = useState([])
 
   let handleWatch = (animeObj) => {
-  setWatch((prev) => {
-    const updated = [...prev, animeObj]
-    console.log(updated)
-    return updated
-  })
-}
-
+    setWatch((prev) => {
+      const updated = [...prev, animeObj]
+      console.log(updated)
+      return updated
+    })
+  }
 
   return (
     <>
-    <BrowserRouter>
-     <Navbar/>
-      <Routes>
-        <Route path='/' element={<><Banner/> <Animes handleWatch={handleWatch}/> </>}/>
-        <Route path='/watchlist' element={<Watchlist/>}/>
-        <Route path='/test' element={<Test/>} />
-        <Route path='/light' element={<Light/>}></Route>
-      </Routes>
-    </BrowserRouter>
-    <Analytics/>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<><Banner/> <Animes handleWatch={handleWatch}/> </>}/>
+          <Route path='/watchlist' element={<Watchlist/>}/>
+          <Route path='/light' element={<Light/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
