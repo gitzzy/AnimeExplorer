04aecb432c72c50e0a0407c6e35b6875de82d037
gitzzy <tyagidevansh10@gmail.com>
2025-09-09 17:58:36 +0530

    Pagination bug fixed

--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -5,9 +5,9 @@ export default function Pagination({handleNext,handlePrev,curPage}) {
     
   return (
     <div className='bg-gray-400 p-4 mt-10 flex justify-center gap-10'>
-      <div onClick={handlePrev} className='hover:cursor-pointer hover: scale-110 duration-500' ><i class="fa-solid fa-backward"></i></div>
+      <div onClick={handlePrev} className='hover:cursor-pointer hover: scale-110 duration-500' >⬅️</div>
       <p>{curPage}</p>
-      <div onClick={handleNext} className='hover:cursor-pointer hover: scale-110 duration-500'><i class="fa-solid fa-forward"></i></div>
+      <div onClick={handleNext} className='hover:cursor-pointer hover: scale-110 duration-500'>➡️</div>
     </div>
   )
 }
